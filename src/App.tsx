b47ff58360282305f4cd42f8500f7efdeea2df6c
Rafael <rafaelemosfr@gmail.com>
2025-11-08 12:08:14 -0300

    Adição de botão de whatsapp

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton';
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
         <Skills />
         <Contact />
         <Footer />
+        <WhatsAppButton />
       </div>
     </div>
   );
